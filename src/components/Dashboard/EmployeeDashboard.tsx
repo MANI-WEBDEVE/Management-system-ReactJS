c5a52bfadf404f21b9e52292cdf998edb20f3602
Muhammad Inam <nudmaufon@gmail.com>
2024-10-17 23:56:19 +0500

    employee-dashboard-ui-alomost-complete

--- src/components/Dashboard/EmployeeDashboard.tsx
+++ src/components/Dashboard/EmployeeDashboard.tsx
@@ -1,12 +1,14 @@
 
+import TaskList from "../TaskList/TaskList"
 import Header from "../UI-Components/Header"
 import TextListCount from "../UI-Components/TextListCount"
 
 const EmployeeDashboard = () => {
   return (
-    <div className="h-screen w-full bg-[#1C1C1C]">
+    <div className="h-full w-full bg-[#1C1C1C] ">
       <Header/>
       <TextListCount/>
+      <TaskList/>
     </div>
   )
 }
