427da42eec1afebc691dab33a82c944a7e6b9724
Muhammad Inam <nudmaufon@gmail.com>
2024-10-17 23:56:58 +0500

    taskList-UI & TaskListCount-UI

--- src/components/UI-Components/TextListCount.tsx
+++ src/components/UI-Components/TextListCount.tsx
@@ -14,11 +14,11 @@ const TextListCount = () => {
         </div>
         <div className="w-screen mr-5 flex justify-evenly max-[579px]:flex-col max-[579px]:gap-3 max-[579px]:mr-2 gap-10"> 
 
-        <div className="rounded-xl w-[95%] py-5 px-12  bg-emerald-400 max-[579px]:w-[70%] ">
+        <div className="rounded-xl w-[95%] py-5 px-12  bg-emerald-400 max-[579px]:w-[90%] ">
             <h2 className="text-5xl font-bold">0</h2>
             <h3 className="text-xl font-semibold mt-6">New Task</h3>
         </div>
-        <div className="max-[579px]:w rounded-xl w-[95%] py-5 px-12  bg-yellow-400 ">
+        <div className="max-[579px]:w rounded-xl w-[90%] py-5 px-12  bg-yellow-400 ">
             <h2 className="text-5xl font-bold">0</h2>
             <h3 className="text-xl font-semibold mt-6">New Task</h3>
         </div>
