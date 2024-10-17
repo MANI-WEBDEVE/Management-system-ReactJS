6fb632b6a5e6b414d6be621916158b51bb6f5e74
Muhammad Inam <nudmaufon@gmail.com>
2024-10-18 00:43:46 +0500

    admin-dashboard-UI

--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -6,7 +6,7 @@ const TaskList = () => {
         <h1 className="text-sm bg-emerald-700 px-2 py-1 rounded-lg font-medium">High</h1>
         <p className="text-sm">2/dec/2024</p>
        </div>
-       <h2 className="text-xl font-bold px-5 uppercase font-semibold">Make a Web Page for a Company</h2>
+       <h2 className="text-xl px-5 uppercase font-semibold">Make a Web Page for a Company</h2>
        <p className="text-sm px-5 mt-5 mb-5 font-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti id earum perspiciatis velit eligendi. Consectetur?</p>
       </div>
       
