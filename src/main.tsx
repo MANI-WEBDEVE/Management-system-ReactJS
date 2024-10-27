45a1229856da13ae2df95f0d399d1e80525ed9ca
Muhammad Inam <nudmaufon@gmail.com>
2024-10-27 23:12:06 +0500

    config-and-setup-shadecn-ui

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-
+import AuthContext from "./context/AuthContext.tsx";
+import TaskContext from "./context/TaskContext.tsx";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <AuthContext>
+      <TaskContext>
+        <App />
+      </TaskContext>
+    </AuthContext>
+  </React.StrictMode>
 );
