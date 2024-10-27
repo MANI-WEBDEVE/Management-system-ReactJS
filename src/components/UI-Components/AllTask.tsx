91a3928706ff59a114e4df58018464682b91c5d3
Muhammad Inam <nudmaufon@gmail.com>
2024-10-27 21:46:21 +0500

    UI-part-is-done

---
+++ src/components/UI-Components/AllTask.tsx
@@ -0,0 +1,29 @@
+
+const AllTask = () => {
+  return (
+    <div className='p-5 bg-[#2b2a2acb] mt-4 rounded-lg w-[95%] flex flex-col gap-10 items-start mb-10 mx-auto h-72 overflow-auto'>
+        <div className='flex flex-col bg-blue-500 w-full py-3 px-10 rounded-lg'>
+            <h1 className='text-2xl font-bold text-white uppercase'>Muhammad Inam</h1>
+            <p className='text-white'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, accusamus?</p>
+        </div>
+        <div className='flex flex-col bg-cyan-500 w-full py-3 px-10 rounded-lg'>
+            <h1 className='text-2xl font-bold text-white uppercase'>Muhammad Inam</h1>
+            <p className='text-white'>Lorem ipsum, dolor sit amet consectetur  amet consectetur amet consectetur amet consectetur amet consectetur amet consectetur amet consectetur amet consectetur amet consectetur amet consectetur amet consectetur amet consectetu?</p>
+        </div>
+        <div className='flex flex-col bg-purple-500 w-full py-3 px-10 rounded-lg'>
+            <h1 className='text-2xl font-bold text-white uppercase'>Muhammad Inam</h1>
+            <p className='text-white'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, accusamus?</p>
+        </div>
+        <div className='flex flex-col bg-yellow-600 w-full py-3 px-10 rounded-lg'>
+            <h1 className='text-2xl font-bold text-white uppercase'>Muhammad Inam</h1>
+            <p className='text-white'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, accusamus?</p>
+        </div>
+        <div className='flex flex-col bg-emerald-700 w-full py-3 px-10 rounded-lg'>
+            <h1 className='text-2xl font-bold text-white uppercase'>Muhammad Inam</h1>
+            <p className='text-white'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, accusamus?</p>
+        </div>
+    </div>
+  )
+}
+
+export default AllTask
---
+++ src/components/UI-Components/CreateTask.tsx
@@ -0,0 +1,84 @@
+const CreateTask = () => {
+  return (
+    <>
+      <div className="w-full bg-[#28262654] mx-auto">
+        <h3 className="text-4xl font-bold text-blue-500 text-center">
+          Create Task
+        </h3>
+        <div className=" h-full flex flex-col ">
+          <form className="w-full flex justify-center items-center gap-10">
+            <div className="w-[20%] flex items-center justify-start m-3 flex-col">
+              <label
+                htmlFor="title"
+                className="text-white flex flex-col justify-center items-start gap-3 font-semibold "
+              >
+                Task
+                <input
+                  type="text"
+                  placeholder="Enter Your Task Title"
+                  className="pr-20 pl-3 py-2 rounded-lg bg-transparent border-blue-500 border-[1px]"
+                />
+              </label>
+              <label
+                htmlFor="desctiption"
+                className="text-white flex flex-col justify-center items-start gap-3 font-semibold "
+              >
+                Description
+                <textarea
+                  name="desctiption"
+                  id="desctiption"
+                  placeholder="Enter Your Task Description"
+                  rows={4}
+                  cols={22}
+                  className="pr-20 pl-3 py-2 rounded-lg bg-transparent border-blue-500 border-[1px]"
+                ></textarea>
+              </label>
+            </div>
+            <div className="w-[20%] flex items-center justify-start m-3 flex-col">
+              <label
+                htmlFor="Date"
+                className="text-white flex flex-col justify-center items-start gap-3 font-semibold "
+              >
+                Date
+                <input
+                  type="text"
+                  placeholder="Enter Your Task Date"
+                  className="pr-20 pl-3 py-2 rounded-lg bg-transparent border-blue-500 border-[1px]"
+                />
+              </label>
+              <label
+                htmlFor="Asign"
+                className="text-white flex flex-col justify-center items-start gap-3 font-semibold "
+              >
+                Asign
+                <input
+                  type="text"
+                  placeholder="Enter Your Task Asign"
+                  className="pr-20 pl-3 py-2 rounded-lg bg-transparent border-blue-500 border-[1px]"
+                />
+              </label>
+              <label
+                htmlFor="Company"
+                className="text-white flex flex-col justify-center items-start gap-3 font-semibold  "
+              >
+                Company
+                <input
+                  type="text"
+                  placeholder="Enter Company Name"
+                  className="pr-20 pl-3 py-2 rounded-lg bg-transparent border-blue-500 border-[1px]"
+                />
+              </label>
+            </div>
+          </form>
+          <div className="flex justify-center">
+            <button className="px-10 py-2 bg-blue-600 font-semibold text-xl mb-10 mt-4 rounded-lg hover:bg-blue-900 hover:text-gray-400 translate-all duration-500">
+              Create Task
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CreateTask;
