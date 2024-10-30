eab3b0f955d30c3f60e08ac1bb3812b388bf16bc
Muhammad Inam <nudmaufon@gmail.com>
2024-10-30 23:47:30 +0500

    some-minner-mistake-inshallah-rebuild-again

--- src/components/Dashboard/EmployeeDashboard.tsx
+++ src/components/Dashboard/EmployeeDashboard.tsx
@@ -6,11 +6,11 @@ import TextListCount from "../UI-Components/TextListCount"
 const EmployeeDashboard = ({dataEmployee, changeUser}:{dataEmployee:any, changeUser:any}) => {
 
   const { tasks, name , taskCount}:{ tasks:any, name:string, taskCount:any} = dataEmployee
-
+ 
   return (
     <div className="h-full w-full bg-[#1C1C1C] ">
       <Header headerData={name} changeUser={changeUser}/>
-      <TextListCount taskCount={taskCount}/>
+      <TextListCount taskCount={tasks}/>
       <TaskList tasks={tasks}/>
     </div>
   )
--- src/components/TaskList/NewTask.tsx
+++ src/components/TaskList/NewTask.tsx
@@ -1,7 +1,35 @@
+import { toast } from 'sonner'
 import { Button } from '../ui/button'
-
+import { useContext } from 'react'
+import { AuthContext } from '@/context/AuthProvider'
 const NewTask = ({data}: {data:any}) => {
+  
   const {title , description, date}: {title:string, description:string, date:string} = data
+  
+  const [userData, setUserData]: any = useContext(AuthContext)
+
+  const handleAcceptTask = () => {
+    const user = JSON.parse(localStorage.getItem("LoggedInUser") as any)
+    if (user.role === 'employees') {
+      userData.employees.map((items:any) => {
+        if (items.name === user.data.name) {
+          user.data.tasks.map((task:any) => {
+            if(task.newTask === true) {
+           
+            // task.newTask = false
+            // task.active = true
+          }
+         })
+        }
+      })
+
+
+
+      toast("Task Accepted", { icon: "✅" })
+    }
+  }
+
+
   return (
     <>
       <div className="bg-indigo-600 h-1/2  w-1/5   rounded-xl  flex-shrink-0 max-[629px]:w-[90%]">
@@ -12,7 +40,7 @@ const NewTask = ({data}: {data:any}) => {
        <h2 className="text-xl px-5 uppercase font-semibold">{title}</h2>
        <p className="text-sm px-5 mt-5 mb-5 font-medium">{description}</p>
        <div className='text-sm font-light text-black flex justify-start mb-5 ml-4'>
-       <Button className='bg-indigo-400 text-black hover:bg-indigo-700 hover:font-semibold transition-all duration-500'>Accepted</Button>
+       <Button onClick={handleAcceptTask} className='bg-indigo-400 text-black hover:bg-indigo-700 hover:font-semibold transition-all duration-500'>Accepted</Button>
        </div>
       </div>
     </>
