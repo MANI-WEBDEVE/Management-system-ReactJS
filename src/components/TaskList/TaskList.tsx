0ed5c33729262ccc47915a0d07601952ee1274b9
Muhammad Inam <nudmaufon@gmail.com>
2024-10-30 23:47:50 +0500

    task-listrender-bugs

--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -4,19 +4,21 @@ import FaliedTask from "./FailedTask";
 import NewTask from "./NewTask";
 
 const TaskList = ({ tasks }: { tasks: any }) => {
+  
   return (
     <div className=" w-full h-full mt-10 flex  p-2 justify-center gap-10 max-[629px]:flex-col overflow-x-auto  flex-wrap">
       {tasks.map((task: any, idx: any) => {
-        if (task.active) {
+       
+        if (task.taskCount.active) {
           return <AcceptTask key={idx} data={task}/>
         }
-        if (task.completed) {
+        if (task.taskCount.completed) {
           return <CompletedTask key={idx} data={task}/>
         } 
-        if(task.newTask){
+        if(task.taskCount.newTask){
           return <NewTask key={idx} data={task}/>
         }
-        if(task.failed){
+        if(task.taskCount.failed){
           return <FaliedTask key={idx} data={task}/>
         }
       })}
